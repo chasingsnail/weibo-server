c4012e051a58e7c92fef6d98474ceb7b093729ae
chasingsnail <wyp402905456@hotmail.com>
2020-03-16 23:17:11 +0800

    fix: 修复单元测试模块无法获取登录状态问题

--- test/user.js/login.test.js
+++ test/user.js/login.test.js
@@ -42,19 +42,38 @@ test('登录', async () => {
 		username,
 		password
 	})
-  expect(res.body.errno).toBe(0)
-  cookie = res.headers['set-cookie'].join(';')
+	expect(res.body.errno).toBe(0)
+	console.log('set cookie ', res.headers['set-cookie'])
+	const resCookie = res.headers['set-cookie'][0]
+
+	const str1 = resCookie.match(/weibo.sid=(\S*);\s/)[0]
+
+	const str2 = resCookie.match(/weibo.sid.sig=(\S*);\s/)[0]
+
+	cookie = str1 + str2
 })
 
-test('删除用户', async() => {
-  const res = await server.post('/api/user/delete').set('cookie', cookie)
-  console.log('del', res.body.errno)
-  expect(res.body.errno).toBe(0)
-})
-
-test('删除后用户名不存在', async () => {
-	const res = await server.post('/api/user/isExist').send({
-		username
-	})
+test('删除用户', async () => {
+	const res = await server.post('/api/user/delete').set('Cookie', cookie)
 	expect(res.body.errno).toBe(0)
 })
+
+// 修改基本信息
+// test('修改基本信息应该成功', async () => {
+// 	const res = await server
+// 			.patch('/api/user/changeInfo')
+// 			.send({
+// 					nickname: '测试昵称',
+// 					city: '测试城市',
+// 					picture: '/test.png'
+// 			})
+// 			.set('cookie', cookie)
+// 	expect(res.body.errno).toBe(0)
+// })
+
+// test('删除后用户名不存在', async () => {
+// 	const res = await server.post('/api/user/isExist').send({
+// 		username
+// 	})
+// 	expect(res.body.errno).toBe(0)
+// })
